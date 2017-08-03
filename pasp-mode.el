6511193677d6113fec1171f476c0db3be242ee15
Henrik Jürges <juerges.henrik@gmail.com>
2017-08-03 22:01:04 +0200

    Update pasp-mode.el

--- pasp-mode.el
+++ pasp-mode.el
@@ -10,7 +10,7 @@
 
 ;; You can redistribute this program and/or modify it under the terms of the GNU
 ;; General Public License as published by the Free Software Foundation; either
-;; GPL version 2 or 3.
+;; version 3 of the License, or (at your option) any later version..
 
 ;;; Commentary:
 
